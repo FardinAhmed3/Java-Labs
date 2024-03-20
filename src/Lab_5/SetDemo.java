1d03845c31051d9eb278df5e97d725743a6d7ca5
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-19 20:21:24 -0400

    Lab 5.13 in progress

--- src/Lab_5/SetDemo.java
+++ src/Lab_5/SetDemo.java
@@ -7,7 +7,7 @@ public class SetDemo {
     public static void main(String[] args) {
         HashSet<String> Set=new HashSet<>();
         System.out.println("========= Set ==========");
-        System.out.println();
+        System.out.println(Set+" ("+Set.isEmpty()+")");
     }
 
 }
