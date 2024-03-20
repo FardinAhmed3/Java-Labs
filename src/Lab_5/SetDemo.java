576a7692e190510847dbb0274da37cc507fd9f4d
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-19 21:25:43 -0400

    Lab 5.13 In Progress

--- src/Lab_5/SetDemo.java
+++ src/Lab_5/SetDemo.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class SetDemo {
     public static void main(String[] args) {
         HashSet<String> Set=new HashSet<>();
-        System.out.println("========= Set ==========");
+        System.out.println("========== Set ==========");
         System.out.println(Set+" ("+Set.isEmpty()+")");
         System.out.println();
 
@@ -51,4 +51,43 @@ Str10·->·[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0
 Str20·->·[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0,·Str10]·(false)↵
 Str30·->·[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0,·Str10]·(false)↵
 Str40·->·[Str90,·Str80↵
+...tr10]·(false)↵
+Str90·->·[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0,·Str10]·(false)↵
+↵
+Membership·testing↵
+------------------↵
+Str0:·true↵
+Str10:·true↵
+Str20:·true↵
+Str30:·true↵
+Str40:·true↵
+Str50:·true↵
+Str60:·true↵
+Str70:·true↵
+Str80:·true↵
+Str90:·true↵
+Str100:·false↵
+Str110:·false↵
+Str120:·false↵
+Str130:·false↵
+Str140:·false↵
+Str150:·false↵
+Str160:·false↵
+Str170:·false↵
+Str180:·false↵
+Str190:·false↵
+↵
+Removing↵
+--------↵
+[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0,·Str10]·(false)·->·Str10↵
+[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20,·Str0]·(false)·->·Str0↵
+[Str90,·Str80,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20]·(false)·->·Str80↵
+[Str90,·Str70,·Str60,·Str50,·Str40,·Str30,·Str20]·(false)·->·Str50↵
+[Str90,·Str70,·Str60,·Str40,·Str30,·Str20]·(false)·->·Str40↵
+[Str90,·Str70,·Str60,·Str30,·Str20]·(false)·->·Str20↵
+[Str90,·Str70,·Str60,·Str30]·(false)·->·Str90↵
+[Str70,·Str60,·Str30]·(false)·->·Str70↵
+[Str60,·Str30]·(false)·->·Str60↵
+[Str30]·(false)·->·Str30↵
+[]·(true)↵
  */
