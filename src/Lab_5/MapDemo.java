b12f15bf36525fed6a0f1be83588eddc70b7d277
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-23 20:59:19 -0400

    MapDemo in progress

---
+++ src/Lab_5/MapDemo.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+//Lab
+
+public class MapDemo {
+    public static void main(String[] args) {
+        
+        System.out.println("====== HashMap =======");
+
+    }
+}
