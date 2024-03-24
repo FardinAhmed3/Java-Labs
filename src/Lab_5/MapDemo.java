4f5ffc0ae174fc4c75f4d56511722f7b78daa0d4
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-23 21:02:49 -0400

    Lab 5.15 In progress

--- src/Lab_5/MapDemo.java
+++ src/Lab_5/MapDemo.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-//Lab
+//Lab 5.15 HashMap
 
 public class MapDemo {
     public static void main(String[] args) {
@@ -9,3 +9,27 @@ public class MapDemo {
 
     }
 }
+
+/*==========·Map·==========↵
+Putting↵
+(Str10:·100)·->·{Str10=100}↵
+(Str11:·121)·->·{Str10=100,·Str11=121}↵
+(Str12:·144)·->·{Str10=100,·Str11=121,·Str12=144}↵
+(Str13:·169)·->·{Str10=100,·Str11=121,·Str12=144,·Str13=169}↵
+(Str14:·196)·->·{Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196}↵
+(Str15:·225)·->·{Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196,·Str15=225}↵
+(Str16:·256)·->·{Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196,·Str15=225,·Str16=256}↵
+(Str17:·289)·->·{Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196,·Str15=225,·Str16=256,·Str17=289}↵
+(Str18:·324)·->·{Str18=324,·Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196,·Str15=225,·Str16=256,·Str17=289}↵
+(Str19:·361)·->·{Str18=324,·Str19=361,·Str10=100,·Str11=121,·Str12=144,·Str13=169,·Str14=196,·Str15=225,·Str16=256,·Str17=289}↵
+↵
+Getting↵
+↵
+Str5·is·not·in·the·map↵
+Str6·is·not·in·the·map↵
+Str7·is·not·in·the·map↵
+Str8·is·not·in·the·map↵
+Str9·is·not·in·the·map↵
+Str10·maps·to·100·in·the·map↵
+Str11·maps·to·121·in·the·map↵
+Str12·ma... */
