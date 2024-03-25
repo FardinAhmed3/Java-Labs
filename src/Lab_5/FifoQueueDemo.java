4209fb534e5d21ef79766676000ee8eb1e120ed5
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-25 02:26:22 -0400

    Update FifoQueueDemo.java

--- src/Lab_5/FifoQueueDemo.java
+++ src/Lab_5/FifoQueueDemo.java
@@ -3,6 +3,11 @@ import java.util.ArrayDeque;
 //Lab 5.8 FIFO Queue Demo usign ArrayDeque
 public class FifoQueueDemo {
     public static void main(String[] args) {
-        
+        ArrayDeque<String> ad=new ArrayDeque<>();
+
+        System.out.println("========== FifoQueue ==========");
+
+
+
     }
 }
