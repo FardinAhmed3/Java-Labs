6988177784ba9af9133b4e26db07aec4d5f70410
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-25 02:35:18 -0400

    Lab 5.9 In Progress

---
+++ src/Lab_5/FifoQueueDemoLL.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+
+//Lab 5.9 FIFO Queue Demo usign LinkedList
+public class FifoQueueDemo {
+    public static void main(String[] args) {
+        LinkedList<String> ad=new LinkedList<>();
+
+        System.out.println("========== FifoQueue ==========");
+        System.out.println(ad+" ("+ad.isEmpty()+")");
+
+        System.out.println();
+        
+        System.out.println("Adding");
+        System.out.println("------");
+
+        for (int i=0;i<100;i+=10){
+            String temp="Str"+i;
+            ad.add(temp);
+            System.out.println(temp+" -> "+ad+" ("+ad.isEmpty()+")");
+        }
+
+        System.out.println();
+
+        System.out.println("Removing");
+        System.out.println("--------");
+        for (int i=0;i<100;i+=10){
+            String temp="Str"+i;
+            System.out.println(ad+" ("+ad.isEmpty()+")"+" -> "+temp);
+            ad.remove(temp);
+        }
+        
+        System.out.println(ad+" ("+ad.isEmpty()+")");
+
+
+    }
+}
