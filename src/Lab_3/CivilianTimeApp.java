66a3e469d24ef5d1c67ab4726975f97858f47d74
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-03-27 12:16:31 -0400

    Submitted version of lab 3.1

---
+++ src/Lab_3/CivilianTimeApp.java
@@ -0,0 +1,7 @@
+public class CivilianTimeApp {
+    public static void main(String[] args) {
+        Time civilianTime = new CivilianTime("11:45pm");
+        
+        TimeApp.demo(civilianTime);
+    }
+}
