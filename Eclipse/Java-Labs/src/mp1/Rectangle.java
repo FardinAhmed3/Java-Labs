698240257114a4f45a3bb9d995f413a5f2a136a5
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-03 02:09:06 -0400

    Lab 6

---
+++ Eclipse/Java-Labs/src/mp1/Rectangle.java
@@ -0,0 +1,38 @@
+package mp1;
+
+
+public class Rectangle implements Shape {
+    int length;
+    int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    
+    @Override
+    public int perimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public int area() {
+        return length * width;
+    }
+
+    @Override
+    public int numSides() {
+        return 4;
+    }
+
+    @Override
+    public boolean isQuadrilateral() {
+        return true;
+    }
+
+    @Override
+    public boolean isEquilateral() {
+        return length == width;
+    }
+}
