5fd76182e4c2a12ec92135a4ec6b899b08ea78df
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-07 20:30:20 -0400

    Submitted version of  Lab 6.2.1

---
+++ src/Lab_6/Square.java
@@ -0,0 +1,32 @@
+//lab 6.2.1
+public class Square implements Shape {
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    public int perimeter() {
+        return 4 * side;
+    }
+
+    public int area() {
+        return side * side;
+    }
+
+    public int numSides() {
+        return 4;
+    }
+
+    public boolean isQuadrilateral() {
+        return true;
+    }
+
+    public boolean isEquilateral() {
+        return true;
+    }
+
+    public String toString() {
+        return "an independently implemented square with a side of " + side;
+    }
+}
