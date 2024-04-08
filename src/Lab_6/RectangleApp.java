234c9f7a6250d11e85528e90d994d12e2c4e3628
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-07 20:27:36 -0400

    Submitted version of Lab 6.1.4

--- src/Lab_6/RectangleApp.java
+++ src/Lab_6/RectangleApp.java
@@ -1,5 +1,7 @@
-//Lab 6.1.3
-
+//lab 6.1.4
 public class RectangleApp {
-    
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(3, 4);
+        ShapeApp.demo(rectangle);
+    }
 }
