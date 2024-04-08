ebafba7440cc71ba49d581b9b4d326e4b3595c8c
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-07 20:27:25 -0400

    Submitted version of Lab 6.1.2

--- src/Lab_6/Rectangle.java
+++ src/Lab_6/Rectangle.java
@@ -1,47 +1,80 @@
-//Lab 6.1.2
+// //Lab 6.1.2
 
+// public class Rectangle implements Shape {
+//     int length;
+//     int width;
+    
+// 	public Rectangle(int length, int width) {
+//         this.length = length;
+//         this.width = width;
+//     }
+    
+//     @Override
+//     public int perimeter() {
+//         return 2 * (length + width);
+//     }
+
+//     @Override
+//     public int area() {
+//         return length * width;
+//     }
+
+//     @Override
+//     public int numSides() {
+//         return 4;
+//     }
+
+//     @Override
+//     public boolean isQuadrilateral() {
+//         return true;
+//     }
+
+//     @Override
+//     public boolean isEquilateral() {
+//         return length == width;
+//     }
+
+
+//     public static void main(String[] args) {
+//         RectangleApp rectangle = new RectangleApp(3, 4);
+        
+//         System.out.println("A rectangle with a width of " + myRectangle.width + " and a height of " + myRectangle.length);
+//         System.out.println("perimeter: " + myRectangle.perimeter());
+//         System.out.println("area: " + myRectangle.area());
+//         System.out.println("isQuadrilateral: " + myRectangle.isQuadrilateral());
+//         System.out.println("isEquilateral: " + myRectangle.isEquilateral());
+//     }
+// }
 public class Rectangle implements Shape {
-    int length;
     int width;
-    
-	public Rectangle(int length, int width) {
-        this.length = length;
+    int height;
+
+    public Rectangle(int width, int height) {
         this.width = width;
+        this.height = height;
     }
-    
-    @Override
+
     public int perimeter() {
-        return 2 * (length + width);
+        return 2 * (width + height);
     }
 
-    @Override
     public int area() {
-        return length * width;
+        return width * height;
     }
 
-    @Override
     public int numSides() {
         return 4;
     }
 
-    @Override
     public boolean isQuadrilateral() {
         return true;
     }
 
-    @Override
     public boolean isEquilateral() {
-        return length == width;
+        return width == height;
     }
 
-
-    public static void main(String[] args) {
-        RectangleApp rectangle = new RectangleApp(3, 4);
-        
-        System.out.println("A rectangle with a width of " + myRectangle.width + " and a height of " + myRectangle.length);
-        System.out.println("perimeter: " + myRectangle.perimeter());
-        System.out.println("area: " + myRectangle.area());
-        System.out.println("isQuadrilateral: " + myRectangle.isQuadrilateral());
-        System.out.println("isEquilateral: " + myRectangle.isEquilateral());
+    public String toString() {
+        return String.format("a rectangle with a width of %d and a height of %d", height, width);
     }
 }
