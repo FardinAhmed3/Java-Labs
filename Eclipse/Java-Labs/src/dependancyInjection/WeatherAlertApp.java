3a3ed44778d6adf1e2f4eeead53e94d5d63a9ad6
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-10 19:36:14 -0400

    Update WeatherAlertApp.java

--- Eclipse/Java-Labs/src/dependancyInjection/WeatherAlertApp.java
+++ Eclipse/Java-Labs/src/dependancyInjection/WeatherAlertApp.java
@@ -3,11 +3,18 @@ package dependancyInjection;
 public class WeatherAlertApp {
 	WeatherAlert alert;
 	
-	
+	public WeatherAlertApp(WeatherAlert alert) {
+		this.alert=alert;
+	}
+	public void processAlert(String message) {
+		System.out.println(message);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WeatherAlert app=new WeatherService();
+		WeatherAlert alert=new WeatherService();
+		WeatherAlertApp app=new WeatherAlertApp (alert);
+		app.processAlert("Dependancy, Injected");
 		
 	}
 
