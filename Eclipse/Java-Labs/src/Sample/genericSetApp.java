4d2c6dc27ee116097adf042f8a916745bd79c89f
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-13 12:35:04 -0400

    Create genericSetApp.java

---
+++ Eclipse/Java-Labs/src/Sample/genericSetApp.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class genericSetApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	genericSet<Integer>gSet=new genericSet<Integer>();
+	
+	gSet.add(10);
+	System.out.println(gSet);
+	}
+
+}
