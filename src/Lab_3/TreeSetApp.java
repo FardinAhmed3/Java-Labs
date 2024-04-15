c08d679bdd32144149381172bfc76c3a3f6c671f
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-15 19:58:20 -0400

    Modifications

--- src/Lab_3/TreeSetApp.java
+++ src/Lab_3/TreeSetApp.java
@@ -1,4 +1,3 @@
-
 import java.util.TreeSet;
 
 //Lab 3.5
---
+++ src/Lab_6/MessageSender.java
@@ -0,0 +1,81 @@
+// // Define the PaymentProcessor interface with a method to process payments
+// public interface PaymentProcessor {
+//     void processPayment(double amount);
+
+//     void cancelPayment(String cancel);
+// }
+
+// // Implement the PaymentProcessor interface with a CreditCardProcessor class
+// public class CreditCardProcessor implements PaymentProcessor {
+//     public void processPayment(double amount) {
+//         System.out.println("Processing credit card payment of $" + amount);
+//     }
+// }
+
+// // Use the PaymentProcessor interface in the PaymentService class
+// public class PaymentService {
+//     private PaymentProcessor paymentProcessor;
+
+//     private PaymentProcessor cancelPayment;
+
+//     public PaymentService(PaymentProcessor paymentProcessor) {
+//         this.paymentProcessor = paymentProcessor;
+//     }
+    
+//     public cancelPayment (PaymentProcessor cancelPayment){
+//         this.cancelPayment=cancelPayment;
+//     }
+    
+//     public void makePayment(double amount) {
+//         paymentProcessor.processPayment(amount);
+//     }
+//     public void cancelPayment(String cancel){
+//         cancelPayment.cancelPayment(cancel);
+//     }
+// }
+
+// // Main class to run the example
+// public class Main {
+//     public static void main(String[] args) {
+//         PaymentProcessor processor = new CreditCardProcessor();
+//         PaymentService service = new PaymentService(processor);
+        
+//         service.makePayment(100.0);
+//     }
+// }
+
+
+// A simple class that represents a message sending service
+public class MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending message: " + message);
+    }
+}
+
+// The NotificationService class that depends on MessageSender
+public class NotificationService {
+    private MessageSender messageSender;
+
+    // Constructor injection of the MessageSender dependency
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void notifyUser(String message) {
+        this.messageSender.sendMessage(message);
+    }
+}
+
+// Main class to demonstrate dependency injection
+public class Main {
+    public static void main(String[] args) {
+        // Creating a MessageSender instance
+        MessageSender sender = new MessageSender();
+        
+        // Injecting the MessageSender into NotificationService
+        NotificationService service = new NotificationService(sender);
+        
+        // Using the NotificationService
+        service.notifyUser("Hello, DI without polymorphism!");
+    }
+}
