e34da36451d6c5f8b8ffb71a62b7c09be692406a
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-16 20:43:50 -0400

    Update ArrayDeque.java

--- src/Lab_7/ArrayDeque.java
+++ src/Lab_7/ArrayDeque.java
@@ -1,10 +1,13 @@
 public class ArrayDeque<E> {
     E[] arr;
     int size,capacity=0;
+    static final int INITIAL_CAPACITY=5;
 
-    public ArrayDeque(int capacity){
-        
-    }
+    @SuppressWarnings("unchecked")
+	ArrayDeque(){
+		this.capacity=INITIAL_CAPACITY;
+		arr=(E [])new Object[capacity];		
+	}
 
 
 }
