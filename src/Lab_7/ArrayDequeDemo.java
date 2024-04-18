7fafc33e334daf078cbcad3e3b515f1285bd69be
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-17 20:02:59 -0400

    Update ArrayDequeDemo.java

--- src/Lab_7/ArrayDequeDemo.java
+++ src/Lab_7/ArrayDequeDemo.java
@@ -1,10 +1,18 @@
 import java.util.Deque;
 
+import javax.swing.event.SwingPropertyChangeSupport;
+
 
 
 public class ArrayDequeDemo {
-    
+    public static void main(String[] args) {
+        
+   
+    ArrayDeque<Integer> AD= new ArrayDeque<>();
+
+    AD.add(2);
+    System.out.println(AD);
 
 
-    
+}
 }
