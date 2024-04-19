6d8301d8bbb547aa49e9a38dff9ed6d65b4dcb00
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-04-19 16:13:18 -0400

    Update ArrayDequeDemo.java

--- src/Lab_7/ArrayDequeDemo.java
+++ src/Lab_7/ArrayDequeDemo.java
@@ -1,8 +1,11 @@
-
 public class ArrayDequeDemo {
     public static void main(String[] args) {
-    ArrayDeque<Integer> ArrayDeque=new ArrayDeque<>();
+        ArrayDeque<Integer> ArrayDeque=new ArrayDeque<>();
     ArrayDeque.addFirst(4);
+
+    for(int i=0;i<10;i++){
+        ArrayDeque.addFirst(i);
+    }
     System.out.println(ArrayDeque);
 
 }
