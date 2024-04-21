becb9c86bbf5d22d46bda997977944c7ac0fd725
Fardin <ahmedfardin077@gmail.com>
2024-04-20 21:59:46 -0400

    Lab 7 In Progress

--- src/Lab_7/ArrayDequeDemo.java
+++ src/Lab_7/ArrayDequeDemo.java
@@ -1,7 +1,7 @@
 public class ArrayDequeDemo {
     public static void main(String[] args) {
         ArrayDeque<Integer> AD=new ArrayDeque<>();
-    //AD.addFirst(4);
+
 
     for(int i=0;i<10;i++){
         AD.addFirst(i);
@@ -10,9 +10,7 @@ public class ArrayDequeDemo {
     for(int i=0;i<10;i++){
         AD.addLast(i);
     }
-    // for(int i=0;i<10;i++){
-    //     AD.addLast(i);
-    // }
+
     System.out.println(AD);
     System.out.println(AD.contains(9));
 
