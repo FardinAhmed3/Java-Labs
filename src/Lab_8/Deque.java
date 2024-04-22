afe64130001b86cd96ed3959a48c91019b8bfd23
Fardin <98803821+FardinAhmed3@users.noreply.github.com>
2024-04-21 20:04:46 -0400

    Update Deque.java

--- src/Lab_8/Deque.java
+++ src/Lab_8/Deque.java
@@ -2,7 +2,7 @@
 import java.util.LinkedList;
 public class Deque {
     void addFirst(int value){}
-    void addLast(){}
+    void addLast(int value){}
     void removeFirst(){}
     void removeLast(){}
     
