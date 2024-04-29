d0205bd05d882c7fed29ef5a65f973c91177eb61
Fardin <98803821+FardinAhmed3@users.noreply.github.com>
2024-04-28 21:18:42 -0400

    Update Deque.java

--- src/Lab_8/Deque.java
+++ src/Lab_8/Deque.java
@@ -1,7 +1,7 @@
 //Lab 8.01
 public class Deque <E> {
     void addFirst(E value){}
-    void addLast(int value){}
+    void addLast(E value){}
     void removeFirst(){}
     void removeLast(){}
     
