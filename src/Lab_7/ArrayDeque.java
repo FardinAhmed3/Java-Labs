f95a320807a31bea2f48fbb83cbf2158a0d3a16a
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-05-18 18:02:48 -0400

    Lab 7 In Progress

--- src/Lab_7/ArrayDeque.java
+++ src/Lab_7/ArrayDeque.java
@@ -1,93 +1,25 @@
-public class ArrayDeque<E> {
+/*  [X] Built in Array container
+    [X] Initial Capacity 5
+    []  CheckCapacity Method
+    []  Single Default Constructor
+    []  Container Generics
+    []  Throw Exceptions
+    []  Array should be treated as ring buffer
+*/
+public class ArrayDeque {
     E[] arr;
-    int size, capacity;
-    int front, rear;
-    static final int INITIAL_CAPACITY = 5;
+    static final int INITIAL_CAPACITY=5;
+    int front, rear,size;
 
-    @SuppressWarnings("unchecked")
-    ArrayDeque() {
-        capacity = INITIAL_CAPACITY;
-        arr = (E[]) new Object[capacity];
-        front = 0;
-        rear = 0;
-        size = 0;
-    }
-    void checkCapacity() {
-        if (size < capacity) {
-            return;
-        }
-        capacity = capacity * 2;
-        @SuppressWarnings("unchecked")
-        E[] temp = (E[]) new Object[capacity];
-        for (int i = 0; i < size; i++) {
-            temp[i] = arr[(front + i) % arr.length]; // Use arr.length for modulus
-        }
-        arr = temp;
-        front = 0;
-        rear = size;
+    ArrayDeque(){
+        arr=new arr[INITIAL_CAPACITY];
+        front =-1;
+        rear=-1;
+        size=0;
     }
 
-    void addFirst(E value) {
-        checkCapacity();
-        front = (front - 1 + capacity) % capacity;
-        arr[front] = value;
-        size++;
-    }
-
-    E removeFirst() {
-        if (size == 0) {
-            return null;
-        }
-        E value = arr[front];
-        front = (front + 1) % capacity;
-        size--;
-        return value;
-    }
-
-    // void removeLast(E Value){
-        
-    // }
-    E removeLast() {
-        if (size == 0) {
-            return null;
-        }
-        rear = (rear - 1 + capacity) % capacity;
-        E value = arr[rear];
-        size--;
-        return value;
-    }
+    
 
 
-    void addLast(E value) {
-        checkCapacity();
-        arr[rear] = value;
-        rear = (rear + 1) % capacity;
-        size++;
-    }
 
-    boolean contains(E elementOfSet) {
-        for (int i = 0; i < size; i++) {
-            if (arr[(front + i) % capacity].equals(elementOfSet)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    int getCapacity() {
-        return capacity;
-    }
-
-    int size() {
-        return size;
-    }
-
-    public String toString() {
-        String result = "{";
-        for (int i = 0; i < size; i++) {
-            result += arr[(front + i) % capacity] + (i < size - 1 ? ", " : "");
-        }
-        result += "}";
-        return result;
-    }
 }
