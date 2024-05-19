1225e324054d0fe848d191f1e017f3a7660c595e
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-05-19 15:46:27 -0400

    Update ArrayDeque_v2.java

---
+++ src/Lab_7/ArrayDeque_v2.java
@@ -0,0 +1,24 @@
+
+
+public class ArrayDeque_v2 {
+
+    E[] arr;
+    int front,rear,size;
+    int INITIAL_CAPACITY=5;
+
+    public ArrayDeque_v2(){
+        arr=new arr[INITIAL_CAPACITY];
+        int size=0;
+        int rear=0;
+        int front=0;
+
+    }
+
+    
+
+
+
+
+
+
+}
