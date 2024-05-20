105e8adb0179a638e0a5602fb3db43bc3ec84b50
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-05-19 20:23:32 -0400

    Submitted version of Lab 8.05

--- src/Lab_8/SC1N.java
+++ src/Lab_8/SC1N.java
@@ -1,19 +1,25 @@
-//Lab 8.04 Singly-Linked, Linear, Two Pointer, Header SC1N
-//Naming in CL is wrong, its  Singly Linked Linear Two Pointer (Head/Tail) with Header node (SL2H)
+//Lab 8.05 Singly-Linked, Circular, One Pointer (Tail) No header
 
-public class SC1N {
+public class SC1N1 {
     public E removeLast() {
         if (isEmpty()) throw new CollectionException("Deque", "removeLast", "empty container");
-        if (header.next == tail) return removeFirst();
-        Node<E> current = header;
+        Node<E> current = tail.next;
+        Node<E> prev = tail;
+    
+        if (current == tail) {
+            tail = null;
+            return current.data;
+        }
+    
         while (current.next != tail) {
             current = current.next;
         }
+    
         E data = tail.data;
+        current.next = tail.next;
         tail = current;
-        tail.next = null;
+        
         return data;
     }
-    
     
 }
